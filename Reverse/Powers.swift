eb0a5181178da76244c751f6114014292fd68e01
chandana-murthy <79270565+chandana-murthy@users.noreply.github.com>
2022-12-24 19:35:24 +0100

    Update Powers.swift

--- Reverse/Powers.swift
+++ Reverse/Powers.swift
@@ -2,28 +2,32 @@
 The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
 01 Test 1
 Input
+1
 Expected output
 1
-1
+
 02 Test 2
 Input
-Expected output
 2
+Expected output
 8
+
 03 Test 3
 Input
-Expected output
 3
+Expected output
 81
+
 04 Test 4
 Input
-Expected output
 4
+Expected output
 1024
+
 05 Test 5
 Input
-Expected output
 5
+Expected output
 15625
 
 */
