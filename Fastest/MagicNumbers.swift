5692de40d5863963fafa16f77d355f9b90229b15
chandana-murthy <79270565+chandana-murthy@users.noreply.github.com>
2023-01-07 15:45:49 +0100

    Create MagicNumbers.swift

---
+++ Fastest/MagicNumbers.swift
@@ -0,0 +1,22 @@
+/* 
+A magic square is a grid of unique numbers where each row, column, and diagonal add up to the same value. You're given a magic square with a single incorrect number. Find the correct number.
+
+In the first test case, for example, the number in the middle should be 5, not 10.
+Input
+Line 1: An integer N: the number of rows and columns the grid has
+Next N lines: N space delimited integers x
+Output
+the correct value for the incorrect value in the square
+Constraints
+3≤N≤6
+0≤x≤100
+Example
+Input
+3
+2 7 6
+9 10 1
+4 3 8
+Output
+5
+*/
+
